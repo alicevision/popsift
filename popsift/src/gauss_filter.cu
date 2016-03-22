f11bb5032414c1b4a3cc7d377a6ce308ee59b1e5
Carsten Griwodz <griff@ifi.uio.no>
2016-03-22 20:26:28 +0100

    gauss filters per level

--- popsift/src/gauss_filter.cu
+++ popsift/src/gauss_filter.cu
@@ -1,3 +1,4 @@
+#include "gauss_filter.h"
 #include "s_pyramid.h"
 #include "debug_macros.h"
 
@@ -7,15 +8,18 @@ using namespace std;
 
 namespace popart {
 
-__device__ __constant__ float d_gauss_filter[32];
+__device__ __constant__ float d_gauss_filter[ GAUSS_ALIGN * GAUSS_LEVELS ];
 
 #ifdef PRINT_GAUSS_FILTER_SYMBOL
 __global__
 void print_gauss_filter_symbol( uint32_t columns )
 {
     printf("Entering print_gauss_filter_symbol\n");
-    for( uint32_t x=0; x<columns; x++ ) {
-        printf("%0.3f ", d_gauss_filter[x] );
+    for( int lvl=0; lvl<GAUSS_LEVELS; lvl++ ) {
+        for( uint32_t x=0; x<GAUSS_ALIGN; x++ ) {
+            printf("%0.3f ", d_gauss_filter[lvl*GAUSS_ALIGN+x] );
+        }
+        printf("\n");
     }
     printf("\n");
 }
@@ -35,40 +39,49 @@ void Pyramid::init_filter( float sigma0, uint32_t levels )
              << endl;
         exit( -__LINE__ );
     }
-    if( levels > 12 )
+    if( levels > GAUSS_LEVELS )
     {
         cerr << __FILE__ << ":" << __LINE__ << ", ERROR: "
-             << " More than 12 levels not supported. Re-size __constant__ array and recompile."
+             << " More than " << GAUSS_LEVELS << " levels not supported. Re-size __constant__ array and recompile."
              << endl;
         exit( -__LINE__ );
     }
 
-    float local_filter[32];
+    float local_filter[ GAUSS_ALIGN * GAUSS_LEVELS ];
     // const int W = GAUSS_SPAN; // no filter wider than 25; 32 is just for alignment
     // assert( W % 2 == 1 ); // filters should be symmetric, i.e. odd-sized
-    // const double mean = GAUSS_ONE_SIDE_RANGE; // is always (GAUSS_SPAN-1)/2
+    // const double mean = the center value
 
     float sigma = sigma0;
-    double sum = 0.0;
-    for (int x = 0; x < GAUSS_SPAN; ++x) {
-            /* Should be:
-             * kernel[x] = exp( -0.5 * (pow((x-mean)/sigma, 2.0) ) )
-             *           / sqrt(2 * M_PI * sigma * sigma);
-             _w /= 2;
-             _h /= 2;
-             * but the denominator is constant and we divide by sum anyway
-             */
-        local_filter[x] = exp( -0.5 * (pow( double(x-GAUSS_ONE_SIDE_RANGE)/sigma, 2.0) ) );
-        sum += local_filter[x];
-    }
+    for( int lvl=0; lvl<GAUSS_LEVELS; lvl++ ) {
+        for (int x = 1; x < GAUSS_ALIGN; ++x) {
+            local_filter[lvl * GAUSS_ALIGN + x] = 0.0;
+        }
 
-    for (int x = 0; x < GAUSS_SPAN; ++x) 
-        local_filter[x] /= sum;
+        local_filter[lvl * GAUSS_ALIGN + 0] = 1.0;
+        double sum = 1.0;
+        for (int x = 1; x <= GAUSS_SPAN; ++x) {
+                /* Should be:
+                 * kernel[x] = exp( -0.5 * (pow((x-mean)/sigma, 2.0) ) )
+                 *           / sqrt(2 * M_PI * sigma * sigma);
+                 * _w /= 2;
+                 * _h /= 2;
+                 * but the denominator is constant and we divide by sum anyway
+                 */
+            local_filter[lvl * GAUSS_ALIGN + x] = exp( -0.5 * (pow( double(x)/sigma, 2.0) ) );
+            sum += 2 * local_filter[lvl * GAUSS_ALIGN + x];
+        }
+
+        for (int x = 0; x <= GAUSS_SPAN; ++x) 
+            local_filter[lvl * GAUSS_ALIGN + x] /= sum;
+
+        sigma *= pow( 2.0, 1.0/levels );
+    }
 
     cudaError_t err;
     err = cudaMemcpyToSymbol( d_gauss_filter,
                               local_filter,
-                              32*sizeof(float),
+                              GAUSS_ALIGN * GAUSS_LEVELS * sizeof(float),
                               0,
                               cudaMemcpyHostToDevice );
     POP_CUDA_FATAL_TEST( err, "cudaMemcpyToSymbol failed for Gauss kernel initialization: " );
