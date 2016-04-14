87615fb7f556b1b5466d8b3da0e61a2e435b6da8
Carsten Griwodz <griff@simula.no>
2016-04-14 12:38:47 +0200

    simplify PopSift class and source

--- popsift/src/SIFT.cu
+++ popsift/src/SIFT.cu
@@ -1,32 +1,18 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <iomanip>
-#include <cmath>
-#include <assert.h>
-
 #include "SIFT.h"
 #include "s_sigma.h"
 #include "gauss_filter.h"
-#include "debug_macros.h"
 #include "write_plane_2d.h"
 
 using namespace std;
 
 PopSift::PopSift( popart::Config config )
-    : _init_octaves( config.octaves )
-    , _levels( max( 2, config.levels ) ) // min is 2, GPU restriction */
-    , _downscale( config.start_sampling )
-    , _sigma( config.sigma )
-    , _threshold( config.threshold ) // SIFT parameter
-    , _edgeLimit( config.edge_limit ) // SIFT parameter
-    , _vlfeat_mode( config.sift_mode == popart::Config::VLFeat )
-    , _log_to_file( config.log_mode == popart::Config::All )
-    , _verbose( config.verbose )
-    , _downscaling_mode( config.scaling_mode )
+    : _config( config )
 {
-    popart::init_filter( _sigma, _levels, _vlfeat_mode );
-    popart::init_sigma(  _sigma, _levels );
+    _config.levels = max( 2, config.levels );
+
+    const bool vlfeat_mode = ( config.sift_mode == popart::Config::VLFeat );
+    popart::init_filter( _config.sigma, _config.levels, vlfeat_mode );
+    popart::init_sigma(  _config.sigma, _config.levels );
 }
 
 PopSift::~PopSift()
@@ -38,13 +24,13 @@ bool PopSift::init( int pipe, int w, int h )
         return false;
     }
 
-    int octaves = _init_octaves;
+    int octaves = _config.octaves;
 
     /* up=-1 -> scale factor=2
      * up= 0 -> scale factor=1
      * up= 1 -> scale factor=0.5
      */
-    float scaleFactor = 1.0 / pow( 2.0, _downscale );
+    float scaleFactor = 1.0 / pow( 2.0, _config.start_sampling );
 
     if( octaves < 0 ) {
         octaves = max(int (floor( logf( (float)min( w, h ) )
@@ -54,10 +40,10 @@ bool PopSift::init( int pipe, int w, int h )
     _pipe[pipe]._inputImage = new popart::Image( w, h );
     _pipe[pipe]._pyramid = new popart::Pyramid( _pipe[pipe]._inputImage,
                                                 octaves,
-                                                _levels,
+                                                _config.levels,
                                                 ceilf( w * scaleFactor ),
                                                 ceilf( h * scaleFactor ),
-                                                _downscaling_mode );
+                                                _config.scaling_mode );
 
     return true;
 }
@@ -81,9 +67,10 @@ void PopSift::execute( int pipe, imgStream inp )
 
     _pipe[pipe]._pyramid->build( _pipe[pipe]._inputImage );
 
-    _pipe[pipe]._pyramid->find_extrema( _edgeLimit, _threshold );
+    _pipe[pipe]._pyramid->find_extrema( _config.edge_limit, _config.threshold );
 
-    if( _log_to_file ) {
+    bool log_to_file = ( _config.log_mode == popart::Config::All );
+    if( log_to_file ) {
         popart::write_plane2D( "upscaled-input-image.pgm",
                                true, // is stored on device
                                _pipe[pipe]._inputImage->getUpscaledImage() );
--- popsift/src/SIFT.h
+++ popsift/src/SIFT.h
@@ -1,35 +1,8 @@
-/**
- * Copyright (c) 2012, Fixstars Corp.
- * All rights reserved.
- * 
- * See license at end of file.
- */
-
-/**
- * @file   SIFT.hpp
- * @author Yuri Ardila <y_ardila@fixstars.com>
- * @date   Tue Oct 30 16:01:19 JST 2012
- * 
- * @brief  
- *    SIFT
- *    OpenCL Implementation
- *
- */
-
 #pragma once
-
-#include <vector>
-#include <iostream>
-#include <assert.h>
 
 #include <cuda_runtime.h>
 
-/* include utilities */
-#include "keep_time.h"
 #include "c_util_img.h"
-#include "plane_2d.h"
-#include "debug_macros.h"
-#include "s_image.h"
 #include "s_pyramid.h"
 #include "sift_conf.h"
 
@@ -46,17 +19,14 @@ class PopSift
 
         popart::Pyramid* _pyramid;
     };
+
 public:
     /* We support more than 1 streams, but we support only one sigma and one
      * level parameters.
      */
     PopSift( popart::Config config );
-
     ~PopSift();
 
-	/**************************/
-	/* @brief SIFT executions */
-	/**************************/
 public:
     bool init( int pipe, int w, int h );
 
@@ -65,90 +35,19 @@ public:
     void uninit( int pipe );
 
 private:
-    size_t           _upscaled_width;  // popart in use
-    size_t           _upscaled_height; // popart in use
-    Pipe             _pipe[MAX_PIPES];
-    // popart::Image*   _baseImg;         // popart in use
-    // popart::Pyramid* _pyramid;         // popart in use
+    Pipe            _pipe[MAX_PIPES];
+    popart::Config& _config;
 
-    // cudaTextureObject_t _texture; // for upscale v5
-    // cudaTextureDesc     _texDesc; // for upscale v5
-    // cudaResourceDesc    _resDesc; // for upscale v5
-
-    int              _init_octaves;    /* number of octaves */
-    const int        _levels;          /* number of levels */
-    const float      _downscale;       /* downscale by 2^this */
-    const float      _sigma;           /* initial sigma */
-    const float      _threshold;       /* DoG threshold */
-    const float      _edgeLimit;       /* edge threshold */
-    const int        _vlfeat_mode;
-    const bool       _log_to_file;
-    const bool       _verbose;
-
-    const popart::Config::ScalingMode _downscaling_mode;
-
-    // popart::Plane2D_uint8 _hst_input_image;
-    // popart::Plane2D_uint8 _dev_input_image;
-    cudaStream_t          _stream;
+    // int              _init_octaves;    /* number of octaves */
+    // const int        _levels;          /* number of levels */
+    // const float      _downscale;       /* downscale by 2^this */
+    // const float      _sigma;           /* initial sigma */
+    // const float      _threshold;       /* DoG threshold */
+    // const float      _edgeLimit;       /* edge threshold */
+    // const int        _vlfeat_mode;
+    // const bool       _log_to_file;
+    // const bool       _verbose;
+    // const popart::Config::ScalingMode _downscaling_mode;
+    // cudaStream_t          _stream;
 };
-
-/**
- * Copyright (c) 2012, Fixstars Corp.
- * All rights reserved.
- * 
- * The following patent has been issued for methods embodied in this 
- * software: "Method and apparatus for identifying scale invariant features 
- * in an image and use of same for locating an object in an image," David 
- * G. Lowe, US Patent 6,711,293 (March 23, 2004). Provisional application 
- * filed March 8, 1999. Asignee: The University of British Columbia. For 
- * further details, contact David Lowe (lowe@cs.ubc.ca) or the 
- * University-Industry Liaison Office of the University of British 
- * Columbia.
- * 
- * Note that restrictions imposed by this patent (and possibly others) 
- * exist independently of and may be in conflict with the freedoms granted 
- * in this license, which refers to copyright of the program, not patents 
- * for any methods that it implements.  Both copyright and patent law must 
- * be obeyed to legally use and redistribute this program and it is not the 
- * purpose of this license to induce you to infringe any patents or other 
- * property right claims or to contest validity of any such claims.  If you 
- * redistribute or use the program, then this license merely protects you 
- * from committing copyright infringement.  It does not protect you from 
- * committing patent infringement.  So, before you do anything with this 
- * program, make sure that you have permission to do so not merely in terms 
- * of copyright, but also in terms of patent law.
- * 
- * Please note that this license is not to be understood as a guarantee 
- * either.  If you use the program according to this license, but in 
- * conflict with patent law, it does not mean that the licensor will refund 
- * you for any losses that you incur if you are sued for your patent 
- * infringement.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are 
- * met:
- *     * Redistributions of source code must retain the above copyright and 
- *       patent notices, this list of conditions and the following 
- *       disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in 
- *       the documentation and/or other materials provided with the 
- *       distribution.
- *     * Neither the name of Fixstars Corp. nor the names of its 
- *       contributors may be used to endorse or promote products derived 
- *       from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS 
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED 
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A 
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
- * HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR 
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
- */
 
