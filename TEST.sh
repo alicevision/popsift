3a720eba95637e8aeea6c69047ba28f331910a51
Carsten Griwodz <griff@simula.no>
2016-06-07 09:37:35 +0200

    move lines in TEST.sh

--- TEST.sh
+++ TEST.sh
@@ -39,7 +39,9 @@ IMAGE7=../sample/level1.ppm
 # PARAMS="--indirect-unfiltered --octaves=4 --threshold=0.1 --edge-threshold=10.0" # finds 4 points
 # PARAMS="--indirect-unfiltered --threshold=0.04 --edge-threshold=10.0" # finds 4 points
 # PARAMS="--downsampling=0 --octaves=4 --indirect-unfiltered --threshold=0.04 --edge-threshold=10.0"
-PARAMS="--downsampling=0 --octaves=4 --indirect-unfiltered --threshold=0.04 --edge-threshold=10.0 --initial-blur=0.5"
+# PARAMS="--downsampling=0 --octaves=4 --indirect-unfiltered --threshold=0.04 --edge-threshold=10.0 --initial-blur=0.5"
+# PARAMS="--vlfeat-mode --sigma=0.82 --octaves=1 --levels=1 --downsampling=0 --indirect-unfiltered --threshold=0.04 --edge-threshold=10.0 --initial-blur=0.5"
+PARAMS="--sigma=1.6 --octaves=4 --levels=3 --downsampling=0 --indirect-unfiltered --threshold=0.0 --edge-threshold=10.0"
 LOG=--log
 # LOG=
 rm -rf 1 2 3 4 5 6 7 dir-*
@@ -48,44 +50,44 @@ echo ./sift_v4 $PARAMS $LOG $IMAGE1
 ./sift_v4 $PARAMS $LOG $IMAGE1
 mkdir 1
 mv dir-* 1/
+cat 1/dir-desc/* > img1_popSIFT.txt
 
 # echo ./sift_v4 $PARAMS $LOG $IMAGE2
 # ./sift_v4 $PARAMS $LOG $IMAGE2
 # mkdir 2
 # mv dir-* 2/
-# 
+# cat 2/dir-desc/* > img2_popSIFT.txt
+
 # echo ./sift_v4 $PARAMS $LOG $IMAGE3
 # ./sift_v4 $PARAMS $LOG $IMAGE3
 # mkdir 3
 # mv dir-* 3/
-# 
+# cat 3/dir-desc/* > img3_popSIFT.txt
+
 # echo ./sift_v4 $PARAMS $LOG $IMAGE4
 # ./sift_v4 $PARAMS $LOG $IMAGE4
 # mkdir 4
 # mv dir-* 4/
-
-echo ./sift_v4 $PARAMS $LOG $IMAGE5
-./sift_v4 $PARAMS $LOG $IMAGE5
-mkdir 5
-mv dir-* 5/
-
-echo ./sift_v4 $PARAMS $LOG $IMAGE6
-./sift_v4 $PARAMS $LOG $IMAGE6
-mkdir 6
-mv dir-* 6/
-
-echo ./sift_v4 $PARAMS $LOG $IMAGE7
-./sift_v4 $PARAMS $LOG $IMAGE7
-mkdir 7
-mv dir-* 7/
-
-cat 1/dir-desc/* > img1_popSIFT.txt
-# cat 2/dir-desc/* > img2_popSIFT.txt
-# cat 3/dir-desc/* > img3_popSIFT.txt
 # cat 4/dir-desc/* > img4_popSIFT.txt
-cat 5/dir-desc/* > img5_popSIFT.txt
-cat 6/dir-desc/* > img6_popSIFT.txt
-cat 7/dir-desc/* > img7_popSIFT.txt
+
+# echo ./sift_v4 $PARAMS $LOG $IMAGE5
+# ./sift_v4 $PARAMS $LOG $IMAGE5
+# mkdir 5
+# mv dir-* 5/
+# cat 5/dir-desc/* > img5_popSIFT.txt
+
+# echo ./sift_v4 $PARAMS $LOG $IMAGE6
+# ./sift_v4 $PARAMS $LOG $IMAGE6
+# mkdir 6
+# mv dir-* 6/
+# cat 6/dir-desc/* > img6_popSIFT.txt
+
+# echo ./sift_v4 $PARAMS $LOG $IMAGE7
+# ./sift_v4 $PARAMS $LOG $IMAGE7
+# mkdir 7
+# mv dir-* 7/
+# cat 7/dir-desc/* > img7_popSIFT.txt
+
 zip -r descriptors.zip img* ?/dir-desc/desc-pyramid-o-*
 
 # output names: <img_name>_popSIFT.txt
