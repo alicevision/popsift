36449bbb37ad65e184c12f2c7ff2f0101c28b49a
Carsten Griwodz <griff@simula.no>
2016-08-01 15:30:22 +0200

    test script

--- TEST.sh
+++ TEST.sh
@@ -60,6 +60,8 @@ IMAGE46=../sample/big_set/trees/img1.ppm
 IMAGE47=../sample/big_set/trees/img5.ppm
 IMAGE48=../sample/big_set/trees/img4.ppm
 
+IMAGE1=../sample/big_set/leuven/img6.ppm
+
 # For testing edge effects on a tiny hand-crafted image
 # IMAGE1=../sample/box-6x6.pgm
 
@@ -89,31 +91,30 @@ mkdir -p outputs/popsift
 
 # for i in $IMAGE1 ; do
 # for i in $IMAGE1 $IMAGE2 $IMAGE3 $IMAGE4 $IMAGE5 $IMAGE6 ; do
-for i in $IMAGE1 $IMAGE2 $IMAGE3 $IMAGE4 $IMAGE5 $IMAGE6 $IMAGE7 $IMAGE8 $IMAGE9 $IMAGE10 $IMAGE11 $IMAGE12 $IMAGE13 $IMAGE14 $IMAGE15 $IMAGE16 $IMAGE17 $IMAGE18 $IMAGE19 $IMAGE20 $IMAGE21 $IMAGE22 $IMAGE23 $IMAGE24 $IMAGE25 $IMAGE22 $IMAGE27 $IMAGE28 $IMAGE29 $IMAGE30 $IMAGE31 $IMAGE32 $IMAGE33 $IMAGE34 $IMAGE35 $IMAGE36 $IMAGE37 $IMAGE38 $IMAGE39 $IMAGE40 $IMAGE41 $IMAGE42 $IMAGE43 $IMAGE44 $IMAGE45 $IMAGE46 $IMAGE47 $IMAGE48 ; do
+for i in $IMAGE1 $IMAGE2 $IMAGE3 $IMAGE4 $IMAGE5 $IMAGE6 $IMAGE7 $IMAGE8 $IMAGE9 $IMAGE10 $IMAGE11 $IMAGE12 $IMAGE13 $IMAGE14 $IMAGE15 $IMAGE16 $IMAGE17 $IMAGE18 $IMAGE19 $IMAGE20 $IMAGE21 $IMAGE22 $IMAGE23 $IMAGE24 $IMAGE25 $IMAGE26 $IMAGE27 $IMAGE28 $IMAGE29 $IMAGE30 $IMAGE31 $IMAGE32 $IMAGE33 $IMAGE34 $IMAGE35 $IMAGE36 $IMAGE37 $IMAGE38 $IMAGE39 $IMAGE40 $IMAGE41 $IMAGE42 $IMAGE43 $IMAGE44 $IMAGE45 $IMAGE46 $IMAGE47 $IMAGE48 ; do
   if [ -f "$i" ] ; then
     Dirname=`dirname $i`
     Dirname=`basename ${Dirname}`
     outname=`basename --suffix=.ppm $i`
     # outname=`basename --suffix=.pgm $i`
-    outname=$Dirname/$outname
     echo ./sift_v4 $PARAMS $LOG $i
     ./sift_v4 $PARAMS $LOG $i
     if [ ! -z "$LOG" ] ; then
-      echo "mkdir -p outputs/${outname}"
-      mkdir -p outputs/${outname}
-      mv dir-* outputs/${outname}/
-      mkdir -p outputs/popsift/$Dirname
-      echo "128" > outputs/popsift/${outname}.sift
-      wc -l  outputs/${outname}/dir-desc/* | tail -1 | awk '{print $1;}' >> outputs/popsift/${outname}.sift
-      cat  outputs/${outname}/dir-desc/* >> outputs/popsift/${outname}.sift
+      echo "mkdir -p outputs/${Dirname}/${outname}"
+      mkdir -p outputs/${Dirname}/${outname}
+      mv dir-* outputs/${Dirname}/${outname}/
+      mkdir -p outputs/${Dirname}/popsift
+      echo "128" > outputs/${Dirname}/popsift/${outname}.sift
+      wc -l  outputs/${Dirname}/${outname}/dir-desc/* | tail -1 | awk '{print $1;}' >> outputs/${Dirname}/popsift/${outname}.sift
+      cat  outputs/${Dirname}/${outname}/dir-desc/* >> outputs/${Dirname}/popsift/${outname}.sift
       echo " "
     fi
   fi
+  if [ ! -z "$LOG" ] ; then
+    ( cd outputs; zip -r popsift.zip ${Dirname}/popsift )
+  fi
 done
 
-if [ ! -z "$LOG" ] ; then
-  ( cd outputs; zip -r popsift.zip popsift )
-fi
 # zip -r descriptors.zip popsift/img* ?/dir-desc/desc-pyramid-o-*
 
 # output names: <img_name>_popSIFT.txt
