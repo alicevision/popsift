76ba6a140f1b9f29e820a1c426e09811eeaa2865
Stian Z. Vrba <vrba@quine.no>
2017-01-20 12:20:06 +0100

    Bugfix in handling of bool_switch arguments.

--- src/application/main.cpp
+++ src/application/main.cpp
@@ -35,67 +35,58 @@ static void parseargs(int argc, char** argv, popsift::Config& config, string& in
     using namespace boost::program_options;
 
     options_description options("Options");
-    {
-        options.add_options()
-            ("help,h", "Print usage")
-            ("verbose,v", bool_switch()->notifier([&](bool) {config.setVerbose(); }), "")
-            ("log,l", bool_switch()->notifier([&](bool) {config.setLogMode(popsift::Config::All); }), "Write debugging files")
-
-            ("input-file,i", value<std::string>(&inputFile)->required(), "Input file")
-            ("match-file,m", value<std::string>(&matchFile), "Match file. If provided PopSIFT will attempt to match the two input-file with match-file.");
+    options.add_options()
+        ("help,h", "Print usage")
+        ("verbose,v", bool_switch()->notifier([&](bool v) { if (v) config.setVerbose(); }), "")
+        ("log,l", bool_switch()->notifier([&](bool v) { if(v) config.setLogMode(popsift::Config::All); }), "Write debugging files")
+        ("input-file,i", value<std::string>(&inputFile)->required(), "Input file")
+        ("match-file,m", value<std::string>(&matchFile), "Match file. If provided PopSIFT will attempt to match the two input-file with match-file.");
     
-    }
+
     options_description parameters("Parameters");
-    {
-        parameters.add_options()
-            ("octaves", value<int>(&config.octaves), "Number of octaves")
-            ("levels", value<int>(&config.levels), "Number of levels per octave")
-            ("sigma", value<float>()->notifier([&](float f) { config.setSigma(f); }), "Initial sigma value")
+    parameters.add_options()
+        ("octaves", value<int>(&config.octaves), "Number of octaves")
+        ("levels", value<int>(&config.levels), "Number of levels per octave")
+        ("sigma", value<float>()->notifier([&](float f) { config.setSigma(f); }), "Initial sigma value")
 
-            ("threshold", value<float>()->notifier([&](float f) { config.setThreshold(f); }), "Contrast threshold")
-            ("edge-threshold", value<float>()->notifier([&](float f) { config.setEdgeLimit(f); }), "On-edge threshold")
-            ("edge-limit", value<float>()->notifier([&](float f) { config.setEdgeLimit(f); }), "On-edge threshold")
-            ("downsampling", value<float>()->notifier([&](float f) { config.setDownsampling(f); }), "Downscale width and height of input by 2^N")
-            ("initial-blur", value<float>()->notifier([&](float f) {config.setInitialBlur(f); }), "Assume initial blur, subtract when blurring first time");
-    }
+        ("threshold", value<float>()->notifier([&](float f) { config.setThreshold(f); }), "Contrast threshold")
+        ("edge-threshold", value<float>()->notifier([&](float f) { config.setEdgeLimit(f); }), "On-edge threshold")
+        ("edge-limit", value<float>()->notifier([&](float f) { config.setEdgeLimit(f); }), "On-edge threshold")
+        ("downsampling", value<float>()->notifier([&](float f) { config.setDownsampling(f); }), "Downscale width and height of input by 2^N")
+        ("initial-blur", value<float>()->notifier([&](float f) {config.setInitialBlur(f); }), "Assume initial blur, subtract when blurring first time");
+
     options_description modes("Modes");
-    {
     modes.add_options()
-        ("gauss-mode", value<std::string>()->notifier([&](const std::string& s) { config.setGaussMode(s); }),
-        "Choice of span (1-sided) for Gauss filters. Default is VLFeat-like computation depending on sigma. Options are: vlfeat, opencv, fixed4, fixed8")
-        ("popsift-mode", bool_switch()->notifier([&](bool) {config.setMode(popsift::Config::PopSift); }),
-        "During the initial upscale, shift pixels by 1. In extrema refinement, steps up to 0.6, do not reject points when reaching max iterations, "
-        "first contrast threshold is .8 * peak thresh. Shift feature coords octave 0 back to original pos.")
-        ("vlfeat-mode", bool_switch()->notifier([&](bool) { config.setMode(popsift::Config::VLFeat); }),
-        "During the initial upscale, shift pixels by 1. That creates a sharper upscaled image. "
-        "In extrema refinement, steps up to 0.6, levels remain unchanged, "
-        "do not reject points when reaching max iterations, "
-        "first contrast threshold is .8 * peak thresh.")
-        ("opencv-mode", bool_switch()->notifier([&](bool b) { if(b) config.setMode(popsift::Config::OpenCV); }),
-        "During the initial upscale, shift pixels by 0.5. "
-        "In extrema refinement, steps up to 0.5, "
-        "reject points when reaching max iterations, "
-        "first contrast threshold is floor(.5 * peak thresh). "
-        "Computed filter width are lower than VLFeat/PopSift")
-
-        ("root-sift", bool_switch()->notifier([&](bool) { config.setUseRootSift(true); }),
-        "Use the L1-based norm for OpenMVG rather than L2-based as in OpenCV")
+        ("popsift-mode", bool_switch()->notifier([&](bool v) { if(v) config.setMode(popsift::Config::PopSift); }),
+            "During the initial upscale, shift pixels by 1. In extrema refinement, steps up to 0.6, do not reject points when reaching max iterations, "
+            "first contrast threshold is .8 * peak thresh. Shift feature coords octave 0 back to original pos.")
+        ("vlfeat-mode", bool_switch()->notifier([&](bool v) { if (v) config.setMode(popsift::Config::VLFeat); }),
+            "During the initial upscale, shift pixels by 1. That creates a sharper upscaled image. "
+            "In extrema refinement, steps up to 0.6, levels remain unchanged, "
+            "do not reject points when reaching max iterations, "
+            "first contrast threshold is .8 * peak thresh.")
+        ("opencv-mode", bool_switch()->notifier([&](bool v) { if (v) config.setMode(popsift::Config::OpenCV); }),
+            "During the initial upscale, shift pixels by 0.5. "
+            "In extrema refinement, steps up to 0.5, "
+            "reject points when reaching max iterations, "
+            "first contrast threshold is floor(.5 * peak thresh). "
+            "Computed filter width are lower than VLFeat/PopSift")
+        ("root-sift", bool_switch()->notifier([&](bool v) { if (v) config.setUseRootSift(true); }),
+            "Use the L1-based norm for OpenMVG rather than L2-based as in OpenCV")
         ("norm-multi", value<int>()->notifier([&](int i) {config.setNormalizationMultiplier(i); }), "Multiply the descriptor by pow(2,<int>).")
-        ("dp-off", bool_switch()->notifier([&](bool) { config.setDPOrientation(false); config.setDPDescriptors(false); }), "Switch all CUDA Dynamic Parallelism off.")
-        ("dp-ori-off", bool_switch()->notifier([&](bool) { config.setDPOrientation(false); }), "Switch DP off for orientation computation")
-        ("dp-desc-off", bool_switch()->notifier([&](bool) { config.setDPDescriptors(false); }), "Switch DP off for descriptor computation");
+        ("dp-off", bool_switch()->notifier([&](bool v) { if (v) config.setDPOrientation(false); config.setDPDescriptors(false); }), "Switch all CUDA Dynamic Parallelism off.")
+        ("dp-ori-off", bool_switch()->notifier([&](bool v) { if (v) config.setDPOrientation(false); }), "Switch DP off for orientation computation")
+        ("dp-desc-off", bool_switch()->notifier([&](bool v) { if (v) config.setDPDescriptors(false); }), "Switch DP off for descriptor computation");
 
-    }
+
     options_description informational("Informational");
-    {
-        informational.add_options()
-        ("print-gauss-tables", bool_switch()->notifier([&](bool) { config.setPrintGaussTables(); }), "A debug output printing Gauss filter size and tables")
+    informational.add_options()
+        ("print-gauss-tables", bool_switch()->notifier([&](bool v) { if (v) config.setPrintGaussTables(); }), "A debug output printing Gauss filter size and tables")
         ("print-dev-info", bool_switch(&print_dev_info)->default_value(false), "A debug output printing CUDA device information")
         ("print-time-info", bool_switch(&print_time_info)->default_value(false), "A debug output printing image processing time after load()")
         ("write-as-uchar", bool_switch(&write_as_uchar)->default_value(false), "Output descriptors rounded to int Scaling to sensible ranges is not automatic, should be combined with --norm-multi=9 or similar");
         
         //("test-direct-scaling")
-    }
 
     options_description all("Allowed options");
     all.add(options).add(parameters).add(modes).add(informational);
