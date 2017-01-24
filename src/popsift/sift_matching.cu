42e8c1edbec91b63c611b2b5f2df6b5f83a899ef
Kristian Skarseth <skarseth@quine.no>
2017-01-24 13:21:46 +0100

    1.1sec

--- src/popsift/sift_matching.cu
+++ src/popsift/sift_matching.cu
@@ -95,7 +95,7 @@ void test(Descriptor* d_desc_a, int desc_a_count, Descriptor* d_desc_b, int desc
     }
 }
 
-//~1,9sec execution
+//~1,5sec execution
 __global__
 void u8_test(U8Descriptor* d_desc_a, int desc_a_count, U8Descriptor* d_desc_b, int desc_b_count, int* output) {
     int tid = threadIdx.x + (blockIdx.x * blockDim.x);
@@ -105,12 +105,24 @@ void u8_test(U8Descriptor* d_desc_a, int desc_a_count, U8Descriptor* d_desc_b, i
     a = d_desc_a[tid];
     float min1 = FLT_MAX, min2 = FLT_MAX;
     int min_index;
-    __shared__ U8Descriptor cached[128];
+    const int cache_size = 32;
+    const int skip_len = cache_size;// *2;
+    __shared__ U8Descriptor cached[cache_size];
     
-    for (int x = 0; x < desc_b_count; x += 128) {
-        cached[threadIdx.x] = d_desc_b[x+threadIdx.x];
-        for (int i = 0; i < 128; i++) {
-            float dst = calc_distance<unsigned char>(&a.features[0], &cached[i].features[0], &min2);
+    for (int x = 0; x < desc_b_count; x += cache_size) {
+        unsigned char* ap = &d_desc_b[x].features[0];
+        unsigned char* bp = &cached[0].features[0];
+        memcpy(bp + (threadIdx.x*skip_len), ap + (threadIdx.x*skip_len), sizeof(unsigned char) * skip_len);
+        //cached[threadIdx.x] = d_desc_b[x+threadIdx.x];
+        for (int i = 0; i < cache_size; i++) {
+            //float dst = calc_distance<unsigned char>(&a.features[0], &cached[i].features[0], &min2);
+            int dst = 0;
+            for (int s = 0; s < 128; s++) {
+                unsigned char sub = a.features[s] - cached[i].features[s];
+                dst += sub*sub;
+
+                //if (dst > min2) break;
+            }
             if (dst < min1) {
                 min2 = min1;
                 min1 = dst;
