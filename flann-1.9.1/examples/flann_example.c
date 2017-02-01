d959cecac0e29aa219887dc30b211abc9650512a
Stian Z. Vrba <vrba@quine.no>
2017-02-01 14:51:35 +0100

    Hard-coded path to test data. Must set # of cores for MSVC OMP.

--- flann-1.9.1/examples/flann_example.c
+++ flann-1.9.1/examples/flann_example.c
@@ -84,9 +84,9 @@ int main(int argc, char** argv)
      * http://people.cs.ubc.ca/~mariusm/uploads/FLANN/datasets/testset.dat
      */
     printf("Reading input data file.\n");
-    dataset = read_points("dataset.dat", rows, cols);
+    dataset = read_points("C:/TEMP/dataset.dat", rows, cols);
     printf("Reading test data file.\n");
-    testset = read_points("testset.dat", tcount, cols);
+    testset = read_points("C:/TEMP/testset.dat", tcount, cols);
     
     nn = 3;
     result = (int*) malloc(tcount*nn*sizeof(int));
@@ -97,12 +97,13 @@ int main(int argc, char** argv)
     p.trees = 8;
     p.log_level = FLANN_LOG_INFO;
 	p.checks = 64;
+    p.cores = 8;    // Needed for MSVC omp
     
     printf("Computing index.\n");
     index_id = flann_build_index(dataset, rows, cols, &speedup, &p);
     flann_find_nearest_neighbors_index(index_id, testset, tcount, result, dists, nn, &p);
     
-    write_results("results.dat",result, tcount, nn);
+    write_results("C:/TEMP/results.dat",result, tcount, nn);
     
     flann_free_index(index_id, &p);
     free(dataset);
