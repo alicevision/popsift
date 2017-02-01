d87ff9cfcef130e578a554a2ce624e9623aea38f
Stian Z. Vrba <stian.vrba@outlook.com>
2017-02-01 19:14:14 +0100

    WIP: table lookup for componentwise hash evaluation.

--- msvc_build/lsh_evaluation/lsh.cpp
+++ msvc_build/lsh_evaluation/lsh.cpp
@@ -7,15 +7,30 @@
 std::mt19937_64 LSHFunction::Engine;
 
 LSHFunction::LSHFunction(unsigned k) :
-    _distribution(0, 32767)
+    _distribution(0, 32767),
+    _dproj(129, 0)
 {
-    // RNG may return duplicates, so use set to generate exactly k different bits.
-    // We also need coordinates to be sorted.
-    std::set<unsigned short> coordinates;
-    while (coordinates.size() < k)
-        coordinates.insert(_distribution(Engine));
-    _coordinates.insert(_coordinates.end(), coordinates.begin(), coordinates.end());
-    if (_coordinates.size() != k)
-        throw std::logic_error("LSHFunction: cardinality < k");
+    _dproj[128] = k;
+    _bits.reserve(k);
+
+    // RNG may return duplicates, so use set to generate exactly k different bits. We also need coordinates to be sorted.
+    {
+        std::set<unsigned short> coordinates;
+        while (coordinates.size() < k)
+            coordinates.insert(_distribution(Engine));
+        _bits.insert(_bits.end(), coordinates.begin(), coordinates.end());
+        if (_bits.size() != k)
+            throw std::logic_error("LSHFunction: cardinality < k");
+    }
+
+    {
+        unsigned i = 0;
+        while (i < k) {
+            unsigned d = _bits[i] >> 8;
+            _dproj[d] = i;
+            while (++i < k && (_bits[i] >> 8) == d)
+                ;
+        }
+    }
 }
 
--- msvc_build/lsh_evaluation/lsh.h
+++ msvc_build/lsh_evaluation/lsh.h
@@ -1,13 +1,15 @@
 #pragma once
 #include <vector>
 #include <random>
+#include <array>
 
 class LSHFunction
 {
     static std::mt19937_64 Engine;  // Common for all instances to ensure continuous stream
 
     std::uniform_int_distribution<int> _distribution;
-    std::vector<unsigned short> _coordinates;
+    std::vector<unsigned short> _dproj;
+    std::vector<unsigned short> _bits;
 
 public:
     LSHFunction(unsigned k);
