ad0a06be04a16a4710fe51943116aec2bb38ea85
Stian Z. Vrba <vrba@quine.no>
2017-02-03 08:28:09 +0100

    Implemented Validate().

--- msvc_build/lsh_evaluation/KDTree.cpp
+++ msvc_build/lsh_evaluation/KDTree.cpp
@@ -1,4 +1,5 @@
 #include "KDTree.h"
+#include <limits>
 
 namespace popsift {
 namespace kdtree {
@@ -10,13 +11,44 @@ KDTree::KDTree(const U8Descriptor* descriptors, size_t dcount) :
     _dcount(dcount),
     _list(dcount)
 {
-    for (size_t i = 0; i < _dcount; ++i)
+    if (dcount > std::numeric_limits<unsigned>::max() / 2)
+        throw std::length_error("KDTree: too many descriptors");
+    for (unsigned i = 0; i < _dcount; ++i)
         _list[i] = i;
 }
 
 void KDTree::Build()
 {
 
+}
+
+// We also check that every element is referenced exactly once by some leaf node.
+// This is done in somewhat hacky way by summing element indices withing a leaf
+// node and comparing the total sum with the expected sum of 0 + 1 + ... + dcount-1.
+void KDTree::Validate()
+{
+    size_t sum = 0;
+
+    for (const Node& n : _nodes) {
+        const size_t lim = n.leaf ? _list.size() : _nodes.size();
+        if (n.left <= n.right)
+            throw std::logic_error("KDTree: unordered links");
+        if (n.left >= lim || n.right >= lim)
+            throw std::logic_error("KDTree: links out of range");
+        if (n.dim >= 128)
+            throw std::logic_error("KDTree: invalid split dimension");
+
+        if (n.leaf)
+        for (auto range = List(n); range.first != range.second; ++range.first) {
+            if (*range.first >= _dcount)
+                throw std::logic_error("KDTree: list element out of range");
+            sum += *range.first;
+        }
+    }
+
+    // Constructor limits count to 2^31, so multiplication won't overflow here.
+    if (sum != (size_t(_dcount) - 1) * size_t(_dcount) / 2)
+        throw std::logic_error("KDTree: not all elements referenced");
 }
 
 }   // kdtree
--- msvc_build/lsh_evaluation/KDTree.h
+++ msvc_build/lsh_evaluation/KDTree.h
@@ -3,6 +3,7 @@
 #include <array>
 #include <vector>
 #include <random>
+#include <utility>
 
 #ifdef _MSC_VER
 #define ALIGNED64 __declspec(align(64))
@@ -12,6 +13,8 @@
 
 namespace popsift {
 namespace kdtree {
+
+static_assert(sizeof(unsigned) == 4, "Unsupported unsigned int size.");
 
 struct U8Descriptor {
     union {
@@ -40,8 +43,9 @@ struct BoundingBox {
 std::array<int, SPLIT_DIMENSION_COUNT> GetSplitDimensions(const U8Descriptor* descriptors, size_t count);
 BoundingBox GetBoundingBox(const U8Descriptor* descriptors, unsigned* indexes, size_t count);
 
+//! KDTree.  Node 0 is the root node.
 class KDTree {
-    struct Link {
+    struct Node {
         unsigned left;      // left link, or begin list index if leaf == 1
         unsigned right;     // right link or end list index if leaf ==1
         unsigned dim : 8;   // splitting dimension
@@ -51,16 +55,26 @@ class KDTree {
     const std::uniform_int_distribution<int> _split_dim_gen;
     const std::uniform_int_distribution<unsigned> _split_val_gen;
     const U8Descriptor *_descriptors;   // Descriptor data
-    const size_t _dcount;               // Count of descriptors
+    const unsigned _dcount;             // Count of descriptors
     std::vector<BoundingBox> _bb;       // BBs of all nodes; packed linearly to not waste cache lines
-    std::vector<Link> _links;           // Link nodes
+    std::vector<Node> _nodes;           // Link nodes
     std::vector<unsigned> _list;        // Elements in leaf nodes; consecutive in range [left,right)
 
 public:
     KDTree(const U8Descriptor* descriptors, size_t dcount);
     KDTree(const KDTree&) = delete;
     KDTree& operator=(const KDTree&) = delete;
+    
     void Build();
+    void Validate();
+
+    const Node& Link(unsigned i) const { return _nodes[i]; }
+    const BoundingBox& BB(unsigned i) const { return _bb[i]; }
+    std::pair<const unsigned*, const unsigned*> List(const Node& node) const {
+        if (node.leaf)
+            throw std::logic_error("KDTree::List: node is not a leaf");
+        return std::make_pair(_list.data() + node.left, _list.data() + node.right);
+    }
 };
 
 
