b7c836c907d779323302b51e1a5a56cace6fe1dd
Carsten Griwodz <griff@simula.no>
2017-07-13 15:20:18 +0200

    intermediate step towards extrema filtering - getting there

--- src/popsift/s_orientation.cu
+++ src/popsift/s_orientation.cu
@@ -11,6 +11,7 @@
 #include <thrust/device_vector.h>
 #include <thrust/sequence.h>
 #include <thrust/transform.h>
+#include <thrust/sort.h>
 
 #include "common/assist.h"
 #include "sift_pyramid.h"
@@ -343,6 +344,41 @@ void ori_prefix_sum( const int total_ext_ct, const int num_octaves )
     }
 }
 
+struct FunctionExtractCell
+{
+    __device__ thrust::tuple<int,float> operator()( const thrust::tuple<int,int>& val) const
+    {
+        const int octave = thrust::get<0>(val);
+        const int idx    = thrust::get<1>(val);
+        InitialExtremum& e = dobuf.i_ext[octave][idx];
+
+        return  thrust::make_tuple( e.cell, e.sigma * powf( 2.0f, octave ) );
+    }
+
+};
+
+struct FunctionReversePosition
+{
+    const int _total;
+    FunctionReversePosition( int total ) : _total(total) { }
+
+    __host__ __device__ int operator()(int val) const
+    {
+        return _total - val - 1;
+    }
+};
+
+struct FunctionIsAbove
+{
+    int _limit;
+    FunctionIsAbove( int limit ) : _limit(limit) { }
+
+    __host__ __device__ bool operator()( int val ) const
+    {
+        return val > _limit;
+    }
+};
+
 __host__
 void Pyramid::extrema_filter_grid( const Config& conf, int ext_total )
 {
@@ -356,21 +392,155 @@ void Pyramid::extrema_filter_grid( const Config& conf, int ext_total )
      */
     const int slots = conf.getGridSize();
 
-    thrust::device_vector<int> octave_index( ext_total );
-    thrust::device_vector<int> iext_index  ( ext_total );
-    thrust::device_vector<int> permutator( ext_total );
-    thrust::device_vector<int> grid   ( ext_total );
+    thrust::device_vector<int>   octave_index( ext_total );
+    thrust::device_vector<int>   iext_index  ( ext_total );
+    thrust::device_vector<int>   cell_values ( ext_total );
+    thrust::device_vector<float> scale_values( ext_total );
+    thrust::device_vector<int>   permutator  ( ext_total );
+    thrust::device_vector<int>   grid        ( ext_total );
+    thrust::device_vector<int>   cell_counts ( slots * slots );
+    thrust::device_vector<int>   cell_offsets( slots * slots );
+
     thrust::sequence( permutator.begin(), permutator.end() );
 
     int sum = 0;
     for( int o=0; o<MAX_OCTAVES; o++ ) {
         const int ocount = hct.ext_ct[o];
         if( ocount > 0 ) {
+            // fill a continuous device array with octave of all initial extrema
             thrust::fill(     octave_index.begin() + sum, octave_index.begin() + sum + ocount, o );
+            // fill a continuous device array with index within octave of all initial extrema
             thrust::sequence( iext_index.begin()   + sum, iext_index  .begin() + sum + ocount );
             sum += ocount;
         }
     }
+
+    FunctionExtractCell fun_extract_cell;
+
+    // extract cell and scale value for all initial extrema
+    thrust::transform( thrust::make_zip_iterator( thrust::make_tuple( octave_index.begin(),
+                                                                      iext_index.begin() ) ),
+                       thrust::make_zip_iterator( thrust::make_tuple( octave_index.end(),
+                                                                      iext_index.end() ) ),
+                       thrust::make_zip_iterator( thrust::make_tuple( cell_values.begin(),
+                                                                      scale_values.begin() ) ),
+                       fun_extract_cell );
+    // sort (octave,index) tuples by their cell values
+    thrust::sort_by_key( cell_values.begin(), cell_values.end(),
+                         thrust::make_zip_iterator( thrust::make_tuple( octave_index.begin(),
+                                                                        iext_index.  begin(),
+                                                                        scale_values.begin() ) ) );
+
+    // count the number of entries in all cells (in one operation instead of several reduce_if)
+    thrust::reduce_by_key( cell_values.begin(), cell_values.end(),
+                           thrust::make_constant_iterator(1),
+                           thrust::make_discard_iterator(),
+                           cell_counts.begin() );
+
+    // compute the offsets from cell_values start for each of the (pre-sorted) cell values
+    thrust::exclusive_scan( cell_counts.begin(), cell_counts.end(), cell_offsets.begin() );
+
+    const int n = slots * slots;
+    thrust::host_vector<int> h_cell_counts         ( n );
+    thrust::host_vector<int> h_cell_permute        ( n );
+    thrust::host_vector<int> cell_count_prefix_sums( n );
+    thrust::host_vector<int> cell_count_sumup      ( n );
+
+    h_cell_counts = cell_counts;
+
+    std::cout << "BEGIN cell value counters" << std::endl;
+    std::copy( h_cell_counts  .begin(), h_cell_counts  .end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "END cell value counters" << std::endl;
+
+    // the cell filter algorithm requires the cell counts in increasing order, cell_permute
+    // maps new position to original index
+    thrust::sequence( h_cell_permute.begin(), h_cell_permute.end() );
+    thrust::sort_by_key( h_cell_counts.begin(), h_cell_counts.end(), h_cell_permute.begin() );
+
+    // several steps to find the cells that must loose extrema
+
+    // inclusive prefix sum
+    thrust::inclusive_scan( h_cell_counts.begin(), h_cell_counts.end(), cell_count_prefix_sums.begin() );
+
+    FunctionReversePosition fun_reverse_pos( n );
+
+    // sumup[i] = prefix sum[i] + sum( cell[i] copied into remaining cells )
+    thrust::transform( h_cell_counts.begin(), h_cell_counts.end(),
+                       thrust::make_transform_iterator( thrust::make_counting_iterator<int>(0),
+                                                        fun_reverse_pos ),
+                       cell_count_sumup.begin(),
+                       thrust::multiplies<int>() );
+    thrust::transform( cell_count_sumup.begin(), cell_count_sumup.end(),
+                       cell_count_prefix_sums.begin(),
+                       cell_count_sumup.begin(),
+                       thrust::plus<int>() );
+
+    FunctionIsAbove function_is_above( conf.getMaxExtrema() );
+
+    // count cells that are above the extrema limit after the summing. Those must share the
+    // reduction of extrema
+    int ct = thrust::count_if( cell_count_sumup.begin(), cell_count_sumup.end(),
+                               function_is_above );
+
+    float tailaverage = float( thrust::reduce( &h_cell_counts[n-ct], &h_cell_counts[n] ) ) / ct;
+
+    int   newlimit    = ::ceilf( tailaverage - ( ext_total - conf.getMaxExtrema() ) / ct );
+
+    // clamp all cells to the computed limit - the total is now less than n extrema off
+    thrust::transform( h_cell_counts.begin(), h_cell_counts.end(), 
+                       thrust::make_constant_iterator<int>(newlimit),
+                       h_cell_counts.begin(),
+                       thrust::minimum<int>() );
+
+    // back to original order
+    thrust::sort_by_key( h_cell_permute.begin(), h_cell_permute.end(), h_cell_counts.begin() );
+
+    // transfer counts back to device
+    cell_counts = h_cell_counts;
+
+    if( 1 ) // filter condition is random selection
+    {
+    }
+    else if( 0 ) // filter condition is largest scale first
+    {
+    }
+    else if( 0 ) // filter condition is smallest scale first
+    {
+    }
+
+#if 1
+    // thrust::host_vector<int> h_octave_index( ext_total );
+    // h_octave_index = octave_index;
+    // std::cout << "BEGIN octave numbers" << std::endl;
+    // std::copy( h_octave_index.begin(), h_octave_index.end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << std::endl;
+    // std::cout << "END octave numbers" << std::endl;
+
+    // thrust::host_vector<int> h_iext_index(   ext_total );
+    // h_iext_index   = iext_index;
+    // std::cout << "BEGIN in-octave index" << std::endl;
+    // std::copy( h_iext_index  .begin(), h_iext_index  .end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << std::endl;
+    // std::cout << "END in-octave index" << std::endl;
+
+    // thrust::host_vector<int> h_cell_values(   ext_total );
+    // h_cell_values   = cell_values;
+    // std::cout << "BEGIN cell values" << std::endl;
+    // std::copy( h_cell_values  .begin(), h_cell_values  .end(), std::ostream_iterator<int>(std::cout, " "));
+    // std::cout << std::endl;
+    // std::cout << "END cell values" << std::endl;
+
+    std::cout << "BEGIN cell value counters" << std::endl;
+    std::copy( h_cell_counts  .begin(), h_cell_counts  .end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "END cell value counters" << std::endl;
+
+    std::cout << "BEGIN original cell index" << std::endl;
+    std::copy( h_cell_permute  .begin(), h_cell_permute  .end(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "END original cell index" << std::endl;
+#endif
 }
 
 __host__
