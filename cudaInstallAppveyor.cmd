29eba0bafac31d3cebf5bd541304cb68ced2df17
Simone Gasparini <simone.gasparini@gmail.com>
2018-05-29 19:49:18 +0200

    [ci] trying cuda 9.1

--- cudaInstallAppveyor.cmd
+++ cudaInstallAppveyor.cmd
@@ -1,18 +1,19 @@
 @echo off
-echo Downloading CUDA toolkit 8
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
-echo Installing CUDA toolkit 8
-cuda_8.0.44_windows.exe -s compiler_8.0 ^
-                           cublas_8.0 ^
-                           cublas_dev_8.0 ^
-                           cudart_8.0 ^
-                           curand_8.0 ^
-                           curand_dev_8.0
+echo Downloading CUDA toolkit 9
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows -FileName cuda_9.1.85_windows.exe
+dir
+echo Installing CUDA toolkit 9
+cuda_9.1.85_windows.exe -s nvcc_9.1 ^
+                           cublas_9.1 ^
+                           cublas_dev_9.1 ^
+                           cudart_9.1 ^
+                           curand_9.1 ^
+                           curand_dev_9.1
 
-echo CUDA toolkit 8 installed
+echo CUDA toolkit 9 installed
 
 dir "%ProgramFiles%"
 
-set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\libnvvp;%PATH%
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\libnvvp;%PATH%
 
 nvcc -V
