46c63a6e53b10588cf408a6cb139307cd9fcd096
Carsten Griwodz <griff@simula.no>
2021-03-10 10:22:28 +0100

    [testing] minor speed improvements by manually finding min1/min2

--- src/compareSiftFiles/csf_feat.cpp
+++ src/compareSiftFiles/csf_feat.cpp
@@ -19,10 +19,8 @@ const float M_PI2 = 2.0f * 3.14159265358979323846f;
 
 bool feat_t::_use_l2_distance = true;
 
-static float dist( const feat_t& l, const feat_t& r )
+inline static float sum_of_squares( const feat_t& l, const feat_t& r )
 {
-    if( feat_t::_use_l2_distance )
-    {
 #ifdef HAVE_STD_TRANSFORM_REDUCE
 /* From C++17 numeric:
 template<class ExecutionPolicy,
@@ -31,7 +29,7 @@ T transform_reduce(ExecutionPolicy&& policy,
                    ForwardIt1 first1, ForwardIt1 last1, ForwardIt2 first2,
                    T init, BinaryOp1 binary_op1, BinaryOp2 binary_op2);
 */
-        const float sum = std::transform_reduce(
+    const float sum = std::transform_reduce(
                                      PAR_UNSEQ
                                      l.desc, l.desc+DESC_SIZE,
                                      r.desc,
@@ -40,14 +38,22 @@ T transform_reduce(ExecutionPolicy&& policy,
                                      [](float l, float r){ float v=l-r; return (v*v); },
                                      std::plus );
 #else
-        float sum = 0.0f;
+    float sum = 0.0f;
 
-        for( int i=0; i<DESC_SIZE; i++ )
-        {
-            const float val = l.desc[i] - r.desc[i];
-            sum += ( val * val );
-        }
+    for( int i=0; i<DESC_SIZE; i++ )
+    {
+        const float val = l.desc[i] - r.desc[i];
+        sum += ( val * val );
+    }
 #endif
+    return sum;
+}
+
+static float dist( const feat_t& l, const feat_t& r )
+{
+    if( feat_t::_use_l2_distance )
+    {
+        const float sum = sum_of_squares( l, r );
         return sqrtf( sum );
     }
     else
@@ -155,65 +161,91 @@ void feat_t::print( ostream& ostr ) const
     }
 }
 
+struct Find2Best
+{
+    float min1 { std::numeric_limits<float>::infinity() };
+    float min2 { std::numeric_limits<float>::infinity() };
+
+    int   idx1;
+    int   idx2;
+
+    inline void update( const int& idx, const float& val )
+    {
+        if( val < min1 )
+        {
+            min2 = min1;
+            idx2 = idx1;
+            min1 = val;
+            idx1 = idx;
+        }
+        else if( val < min2 )
+        {
+            min2 = val;
+            idx2 = idx;
+        }
+    }
+
+    void takeSqrt( )
+    {
+        min1 = sqrtf( min1 );
+        min2 = sqrtf( min2 );
+    }
+};
+
 void feat_t::compareBestMatch( ostream& ostr, ostream* dstr, const vector<feat_t>& l_one, vector<float>& desc_stats,  bool minOnly ) const
 {
     const int l_one_sz = l_one.size();
-
-    vector<float> distances( l_one_sz );
 
     if( !minOnly ) ostr << "==========" << endl;
 
     const feat_t& left( *this );
 
-    for( int j=0; j<l_one_sz; j++ )
-    {
-        distances[j] = dist( left, l_one[j] );
-    }
-
-    auto m = min_element( PAR_UNSEQ
-                          distances.begin(),
-                          distances.end() );
-
     if( minOnly )
     {
-        auto                 it       = distances.begin();
-        float                second   = INFINITY;
-        const vector<float>& left     = desc;
+        Find2Best find2best;
 
         for( int j=0; j<l_one_sz; j++ )
         {
-            const feat_t& r = l_one[j];
-
-            if( it == m )
-            {
-                ostr << "desc dist " << *it
-                     << " MIN"
-                     << " pixdist " << sqrtf( (x-r.x)*(x-r.x) + (y-r.y)*(y-r.y) )
-                     << " angledist " << fabsf( ori/M_PI2*360.0f - r.ori/M_PI2*360.0f );
-
-                if( dstr )
-                {
-                    auto right = r.desc;
-                    for( int i=0; i<DESC_SIZE; i++ )
-                    {
-                        const float d = left[i] - right[i];
-                        (*dstr) << d << " ";
-                        desc_stats[i] += d;
-                    }
-                    (*dstr) << endl;
-                }
-            }
-            else if( *it > *m )
-            {
-                second = min<float>( second, *it );
-            }
-            it++;
+            const float val =  sum_of_squares( left, l_one[j] );
+            find2best.update( j, val );
         }
 
-        ostr << " 2best " << second << endl;
+        find2best.takeSqrt();
+
+        const feat_t& r = l_one[find2best.idx1];
+
+        ostr << "desc dist " << find2best.min1
+             << " MIN"
+             << " pixdist " << sqrtf( (x-r.x)*(x-r.x) + (y-r.y)*(y-r.y) )
+             << " angledist " << fabsf( ori/M_PI2*360.0f - r.ori/M_PI2*360.0f );
+
+        if( dstr )
+        {
+            auto right = r.desc;
+            for( int i=0; i<DESC_SIZE; i++ )
+            {
+                const float d = left.desc[i] - right[i];
+                (*dstr) << d << " ";
+                desc_stats[i] += d;
+            }
+            (*dstr) << endl;
+        }
+
+        ostr << " 2best " << find2best.min2 << endl;
     }
     else
     {
+        vector<float> distances( l_one_sz );
+
+        for( int j=0; j<l_one_sz; j++ )
+        {
+            distances[j] = dist( left, l_one[j] );
+        }
+
+        auto m = min_element( PAR_UNSEQ
+                              distances.begin(),
+                              distances.end() );
+
         auto it = distances.begin();
 
         for( int j=0; j<l_one_sz; j++ )
